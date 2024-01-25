c128028a0b075f1921f7506cfe7076a2a721cc74
Kevin Malats <desarrollo.externokmq@crecoscorp.com>
2024-01-25 15:19:55 -0500

    lklK

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,5 +3,5 @@ type IEnvironment = {
 };
 
 export const environtment: IEnvironment = {
-  url: "https://backend-deploy-vercel-47bg-8wvm5s7wv-kevinmalats.vercel.app/",
+  url: "https://backend-deploy-vercel-47bg-8wvm5s7wv-kevinmalats.vercel.app",
 };
