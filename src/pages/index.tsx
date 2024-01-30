cb03216d3f15eb9db60c19d4780ddf76a8be4211
Kevin Malats <desarrollo.externokmq@crecoscorp.com>
2024-01-29 19:28:48 -0500

    test useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-use "client"
 import React, {useState, useEffect, useRef} from "react";
 import PokemonList from "~/components/pokemonsList";
 import { IPokemSave, PokemonDTO, RequestPostSave } from "~/types/IPokemon";
